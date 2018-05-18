1e16e5cecf954f61c254a8dda8052d39d2bdc156
Muhammad Nasir <nasir031@gmail.com>
2018-05-18 17:05:53 +0500

    devise setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def search
+    render 'layouts/search'
+  end
+
+  # protected  
+  #   def after_sign_in_path_for(resource)
+  #     redirect_to :search
+  #   end
 end

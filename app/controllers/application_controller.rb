10c90c0b634c5442856169c949e616eb302704b9
salmanjavaid05 <sonusalman05@gmail.com>
2018-05-22 10:46:58 +0500

    one layer deeper search done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,12 @@ class ApplicationController < ActionController::Base
   end
 
   def search_results
-    results = RubyInstagramScraper.search(params[:tag])
-    @hash_tags = results['hashtags'].pluck('hashtag').pluck('name', 'media_count')
-    # binding.pry
+    results = RubyInstagramScraper.search(params[:tag])['hashtags'].pluck('hashtag').pluck('name')
+    @hash_tags = []
+    results.each do |result|
+      @hash_tags = @hash_tags + RubyInstagramScraper.search(result)['hashtags'].pluck('hashtag').pluck('name', 'media_count') if result.ascii_only?
+    end
+    @hash_tags = @hash_tags.uniq
     @hash_tags = @hash_tags.sort_by(&:last).reverse
     render 'layouts/search'
   end

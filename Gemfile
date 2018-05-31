a843f40aed6589e5c710ad541455ce057987ccf9
Muhammad Nasir <nasir031@gmail.com>
2018-05-31 17:14:06 +0500

    Added pruning

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'ruby-instagram-scraper'
 gem 'bootstrap'
 gem 'devise'
 gem 'jquery-rails'
+gem 'watir-nokogiri'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use postgresql as the database for Active Record
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'insta_helper.rb'
 class ApplicationController < ActionController::Base
   before_action :set_min_max
 
@@ -6,6 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def search_results
+    # binding.pry
     results = RubyInstagramScraper.search(params[:tag])['hashtags'].pluck('hashtag')
     if params[:post_range].present?
       range = params[:post_range].split(';').map(&:to_i)
@@ -17,12 +19,18 @@ class ApplicationController < ActionController::Base
     results.each do |result|
       @hash_tags += RubyInstagramScraper.search(result)['hashtags'].pluck('hashtag').pluck('name', 'media_count') if result.ascii_only?
     end
-    @hash_tags = @hash_tags.uniq { |s| s.first }
     # binding.pry
+    @hash_tags = @hash_tags.uniq { |s| s.first }
     if params[:post_range].present?
       range = params[:post_range].split(';').map(&:to_i)
       @hash_tags = @hash_tags.select { |_,e| e.between?(range.first, range.last)  }
     end
+
+    
+    # make query to tag 
+    # get 
+    # if tag has a post within last 30 days
+    @hash_tags = tag_pruning @hash_tags if params[:prune].present?
     @hash_tags = @hash_tags.sort_by(&:last).reverse
     render 'layouts/search'
   end
@@ -31,6 +39,16 @@ class ApplicationController < ActionController::Base
     # def after_sign_in_path_for(resource)
     #   redirect_to :search
     # end
+    def tag_pruning tags
+      pruned_tags = []
+      tags.each do |tag|
+        tag_nodes = InstaHelper.get_tag_media_nodes(tag.first)
+        unless (DateTime.now  - DateTime.strptime(tag_nodes.first["node"]["taken_at_timestamp"].to_s,'%s')).to_i > 30
+          pruned_tags.push tag
+        end
+      end
+      pruned_tags
+    end
 
     def set_min_max
       @web_settings = WebSetting.first
